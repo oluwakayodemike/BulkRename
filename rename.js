88cb22d5e26ff51f985226c78e7126de7ca8dc47
Oluwakayode Mike <michaelaykay@gmail.com>
2024-02-04 11:39:37 -0800

    return error, if not found

--- rename.js
+++ rename.js
@@ -25,25 +25,40 @@ rl.question('Enter path to the folder: ', (directoryPath) => {
 
           if (position.toLowerCase() === 'beginning') {
             // Remove from beginning of file name
-            newFileName = file.startsWith(textToRemove) ? file.slice(textToRemove.length) : file;
+            if (file.startsWith(textToRemove)) {
+              newFileName = file.slice(textToRemove.length);
+            } else {
+              // keyword not found at the beginning err
+              console.error(`Error: Keyword "${textToRemove}" not found at the beginning of ${file}.`);
+              return;
+            }
           } else if (position.toLowerCase() === 'end') {
-            //  orend of the file name!
-            newFileName = file.endsWith(textToRemove) ? file.slice(0, -textToRemove.length) : file;
+            //  or end of the file name!
+            if (file.endsWith(textToRemove)) {
+              newFileName = file.slice(0, -textToRemove.length);
+            } else {
+              // keyword not found at the end err
+              console.error(`Error: Keyword "${textToRemove}" not found at the end of ${file}.`);
+              return;
+            }
           } else {
             console.error('Invalid. You can specify only "beginning" or "end".');
             return;
           }
 
-          const newFilePath = path.join(directoryPath, newFileName);
+          // Check if the new file name is different from the old one before renaming
+          if (file !== newFileName) {
+            const newFilePath = path.join(directoryPath, newFileName);
 
-          // Renaming file
-          fs.rename(oldFilePath, newFilePath, (renameErr) => {
-            if (renameErr) {
-              console.error(`Error renaming ${file}:`, renameErr);
-            } else {
-              console.log(`Renamed ${file} to ${newFileName}`);
-            }
-          });
+            // Renaming file
+            fs.rename(oldFilePath, newFilePath, (renameErr) => {
+              if (renameErr) {
+                console.error(`Error renaming ${file}:`, renameErr);
+              } else {
+                console.log(`Renamed ${file} to ${newFileName}`);
+              }
+            });
+          }
         });
       });
     });
